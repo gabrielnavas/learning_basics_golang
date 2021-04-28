ebf655f616b3372c6b39125424b9ae8328d22b4f
Navas <gabrielnavas@email.com>
2021-04-27 22:10:04 -0300

    types_unsigned_integer

--- fundamentals/types_unsigned_integer.go
+++ fundamentals/types_unsigned_integer.go
@@ -7,26 +7,34 @@ import (
 )
 
 func main() {
-	// unsigned byte == uint8, uint16, uint32, uint64
 	// unsigned byte
 	var b byte = 255
 
 	// unsigned uint8
-	var integer_8b uint8 = 255
+	// 255
+	var integer_8b uint8 = math.MaxUint8
 
 	// unsigned uint16
-	var integer_16b uint16 = uint16(math.Pow(float64(integer_8b), 2))
+	// 65535
+	var integer_16b uint16 = math.MaxUint16
 
 	// unsigned uint32
-	var integer_32b uint32 = uint32(math.Pow(float64(integer_16b), 2))
+	// 4294967295
+	var integer_32b uint32 = math.MaxUint32
+
+	// map of the Unicode table  (int32)
+	var int32Alias rune = 'a'
+	fmt.Println(int32Alias) // code 97 letter 'a' from unicode table
 
 	// unsigned uint64
-	var integer_64b uint64 = uint64(math.Pow(float64(integer_32b), 2))
+	// 18446744073709551615
+	var integer_64b uint64 = math.MaxUint64
 
 	// type prints
-	fmt.Println(reflect.TypeOf(b))
-	fmt.Println(reflect.TypeOf(integer_8b))
-	fmt.Println(reflect.TypeOf(integer_16b))
-	fmt.Println(reflect.TypeOf(integer_32b))
-	fmt.Println(reflect.TypeOf(integer_64b))
+	fmt.Println(reflect.TypeOf(b))           // uint8
+	fmt.Println(reflect.TypeOf(integer_8b))  // uint8
+	fmt.Println(reflect.TypeOf(integer_16b)) // uint16
+	fmt.Println(reflect.TypeOf(integer_32b)) // uint32
+	fmt.Println(reflect.TypeOf(int32Alias))  // uint32
+	fmt.Println(reflect.TypeOf(integer_64b)) // uint64
 }
