64a51db18b49a6669906db7d6fc05217feeb8f41
Navas <gabrielnavas@email.com>
2021-05-09 19:17:28 -0300

    init in each file

---
+++ functions/init/init.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// to execute this example, use: go run *.go
+func init() {
+	fmt.Println("I am initing in main file...")
+}
+
+func main() {
+
+}
---
+++ functions/init/otherfile.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("I am other file initing...")
+}
